09b85104a1382d2406533a1a3fe7fb3ad73fd00a
Bert54 <matt.bert@live.fr>
2020-01-24 09:40:42 +0100

    Repository and project initialization

---
+++ cmake-build-debug/CMakeFiles/Projet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet.dir/main.cpp.o"
+  "Projet"
+  "Projet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
